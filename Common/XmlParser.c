6a26ad03b42221532bce0b50e89e301db720fa88
Ran Feder <ranfeder@outlook.com>
2015-08-21 18:33:42 +0300

    4. XmlParser.c compilation

--- Common/XmlParser.c
+++ Common/XmlParser.c
@@ -5,7 +5,7 @@ Settings loadSettings(Settings previousSettings, char* filePath) {
 	xmlNode* rootElement = NULL;
 	Settings settings = DEFAULT_SETTINGS;
 	if ((doc = xmlReadFile(filePath, NULL, 0)) == NULL ||
-		)rootElement = xmlDocGetRootElement(doc)) == NULL) { //root element is <game>
+		(rootElement = xmlDocGetRootElement(doc)) == NULL) { //root element is <game>
 		fprintf(stderr, "error: could not parse file %s\n", filePath);
 		return previousSettings;
 	}
@@ -18,9 +18,9 @@ Settings loadSettings(Settings previousSettings, char* filePath) {
 			else if (!strcmp((char*)gameChild->name, "game_mode"))
 				settings.gameMode = (*gameChild->content) - '0'; //should be '1' or '2'
 			else if (!strcmp((char*)gameChild->name, "difficulty"))
-				settings.minimaxDepth = parseDifficulty(gameChild->content);
+				settings.minimaxDepth = parseDifficulty((char*)gameChild->content);
 			else if (!strcmp((char*)gameChild->name, "user_color"))
-				settings.userColor = parseColor(gameChild->content);
+				settings.userColor = parseColor((char*)gameChild->content);
 			else if (!strcmp((char*)gameChild->name, "board"))
 				settings.board = parseXmlBoard(settings.board, gameChild->children);
 		}
@@ -35,7 +35,7 @@ Settings loadSettings(Settings previousSettings, char* filePath) {
 
 char** parseXmlBoard(char** board, xmlNode* row) {
 	while (row) {
-		if (gameChild->type == XML_ELEMENT_NODE) {
+		if (row->type == XML_ELEMENT_NODE) {
 			if (!strcmp((char*)row->name, "row_1"))
 				readXmlBoardRow(board[0], row->content);
 			else if (!strcmp((char*)row->name, "row_2"))
@@ -55,10 +55,11 @@ char** parseXmlBoard(char** board, xmlNode* row) {
 		}
 		row = row->next;
 	}
+	return board;
 }
 
 void readXmlBoardRow(char* boardRow, xmlChar* xmlRow) {
-	if (xmlRow == NULL || xmlStrLen(xmlRow) < BOARD_SIZE)
+	if (xmlRow == NULL || xmlStrlen(xmlRow) < BOARD_SIZE)
 		return;
 	for (int i = 0; i < BOARD_SIZE; i++) {
 		boardRow[i] = (char)xmlRow[i] == EMPTY ? EMPTY : (char)xmlRow[i];
