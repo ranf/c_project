d11bf48d8778831eb6891926ed03d0053ca98869
Ran Feder <ranfeder@outlook.com>
2015-09-12 18:11:01 +0300

    fix Build.c

--- Gui/Build.c
+++ Gui/Build.c
@@ -6,18 +6,18 @@ gui_chess build_main_menu()
 {
 	gui_chess result, tmp, tmp_parent;
 
-	result = window_factory(backround); /* set backround image */
+	result = window_factory(getImage(BACKGROUND_IMAGE); /* set backround image */
 	tmp = result;
-	tmp->child = panel_factory(panels_sheet, FISH_PANEL_PMT, tmp);
+	tmp->child = panel_factory(getImage(PANELS_SHEET), FISH_PANEL_PMT, tmp);
 	tmp = tmp->child;
-	tmp->child = label_factory(settings_sheet,MAIN_LABEL_PMT,tmp);
+	tmp->child = label_factory(getImage(SETTINGS_SHEET),MAIN_LABEL_PMT,tmp);
 	tmp_parent = tmp;
 	tmp = tmp->child;
-	tmp->next = button_factory(settings_sheet, NEW_BUTTON_PMT, tmp_parent);
+	tmp->next = button_factory(getImage(SETTINGS_SHEET), NEW_BUTTON_PMT, tmp_parent);
 	tmp = tmp->next;
-	tmp->next = button_factory(settings_sheet, LOAD_BUTTON_PMT, tmp_parent);
+	tmp->next = button_factory(getImage(SETTINGS_SHEET), LOAD_BUTTON_PMT, tmp_parent);
 	tmp = tmp->next;
-	tmp->next = button_factory(settings_sheet, QUIT_BUTTON_PMT, tmp_parent);
+	tmp->next = button_factory(getImage(SETTINGS_SHEET), QUIT_BUTTON_PMT, tmp_parent);
 
 	return (result);
 }
@@ -26,20 +26,20 @@ gui_chess build_mode_menu()
 {
 	gui_chess result, tmp, tmp_parent;
 
-	result = window_factory(backround);
+	result = window_factory(getImage(BACKGROUND_IMAGE));
 	tmp = result;
-	tmp->child = panel_factory(panels_sheet, FISH_PANEL_PMT, tmp);
+	tmp->child = panel_factory(getImage(PANELS_SHEET), FISH_PANEL_PMT, tmp);
 	tmp = tmp->child;
-	tmp->child = label_factory(settings_sheet, MODE_LABEL_PMT, tmp);
+	tmp->child = label_factory(getImage(SETTINGS_SHEET), MODE_LABEL_PMT, tmp);
 	tmp_parent = tmp;
 	tmp = tmp->child;
-	tmp->next = button_factory(settings_sheet, PVP_S_BUTTON_PMT, tmp_parent);
+	tmp->next = button_factory(getImage(SETTINGS_SHEET), PVP_S_BUTTON_PMT, tmp_parent);
 	tmp = tmp->next;
-	tmp->next = button_factory(settings_sheet, PVC_BUTTON_PMT, tmp_parent);
+	tmp->next = button_factory(getImage(SETTINGS_SHEET), PVC_BUTTON_PMT, tmp_parent);
 	tmp = tmp->next;
-	tmp->next = button_factory(settings_sheet, CANCEL_BUTTON_PMT, tmp_parent);
+	tmp->next = button_factory(getImage(SETTINGS_SHEET), CANCEL_BUTTON_PMT, tmp_parent);
 	tmp = tmp->next;
-	tmp->next = button_factory(settings_sheet, START_BUTTON_PMT, tmp_parent);
+	tmp->next = button_factory(getImage(SETTINGS_SHEET), START_BUTTON_PMT, tmp_parent);
 
 	return (result);
 }
@@ -48,20 +48,20 @@ gui_chess build_color_set_menu()
 {
 	gui_chess result, tmp, tmp_parent;
 
-	result = window_factory(backround);
+	result = window_factory(getImage(BACKGROUND_IMAGE));
 	tmp = result;
-	tmp->child = panel_factory(panels_sheet, FISH_PANEL_PMT, tmp);
+	tmp->child = panel_factory(getImage(PANELS_SHEET), FISH_PANEL_PMT, tmp);
 	tmp = tmp->child;
-	tmp->child = label_factory(settings_sheet, COLOR_LABEL_PMT, tmp);
+	tmp->child = label_factory(getImage(SETTINGS_SHEET), COLOR_LABEL_PMT, tmp);
 	tmp_parent = tmp;
 	tmp = tmp->child;
-	tmp->next = button_factory(settings_sheet, WHITE_S_BUTTON_PMT, tmp_parent);
+	tmp->next = button_factory(getImage(SETTINGS_SHEET), WHITE_S_BUTTON_PMT, tmp_parent);
 	tmp = tmp->next;
-	tmp->next = button_factory(settings_sheet, BLACK_BUTTON_PMT, tmp_parent);
+	tmp->next = button_factory(getImage(SETTINGS_SHEET), BLACK_BUTTON_PMT, tmp_parent);
 	tmp = tmp->next;
-	tmp->next = button_factory(settings_sheet, CANCEL_BUTTON_PMT, tmp_parent);
+	tmp->next = button_factory(getImage(SETTINGS_SHEET), CANCEL_BUTTON_PMT, tmp_parent);
 	tmp = tmp->next;
-	tmp->next = button_factory(settings_sheet, NEXT_BUTTON_PMT, tmp_parent);
+	tmp->next = button_factory(getImage(SETTINGS_SHEET), NEXT_BUTTON_PMT, tmp_parent);
 
 	return result;
 }
@@ -70,26 +70,26 @@ gui_chess build_diffculty_menu()
 {
 	gui_chess result, tmp, tmp_parent;
 
-	result = window_factory(backround);
+	result = window_factory(getImage(BACKGROUND_IMAGE));
 	tmp = result;
-	tmp->child = panel_factory(panels_sheet, FISH_PANEL_PMT, tmp);
+	tmp->child = panel_factory(getImage(PANELS_SHEET), FISH_PANEL_PMT, tmp);
 	tmp = tmp->child;
-	tmp->child = label_factory(settings_sheet, DIFFICULTY_LABEL_PMT, tmp_parent);
+	tmp->child = label_factory(getImage(SETTINGS_SHEET), DIFFICULTY_LABEL_PMT, tmp_parent);
 	tmp_parent = tmp;
 	tmp = tmp->child;
-	tmp->next = button_factory(settings_sheet, ONE_S_BUTTON_PMT, tmp_parent);
+	tmp->next = button_factory(getImage(SETTINGS_SHEET), ONE_S_BUTTON_PMT, tmp_parent);
 	tmp = tmp->next;
-	tmp->next = button_factory(settings_sheet, TWO_BUTTON_PMT, tmp_parent);
+	tmp->next = button_factory(getImage(SETTINGS_SHEET), TWO_BUTTON_PMT, tmp_parent);
 	tmp = tmp->next;
-	tmp->next = button_factory(settings_sheet, THREE_BUTTON_PMT, tmp_parent);
+	tmp->next = button_factory(getImage(SETTINGS_SHEET), THREE_BUTTON_PMT, tmp_parent);
 	tmp = tmp->next;
-	tmp->next = button_factory(settings_sheet, FOUR_BUTTON_PMT, tmp_parent);
+	tmp->next = button_factory(getImage(SETTINGS_SHEET), FOUR_BUTTON_PMT, tmp_parent);
 	tmp = tmp->next;
-	tmp->next = button_factory(settings_sheet, BEST_BUTTON_PMT, tmp_parent);
+	tmp->next = button_factory(getImage(SETTINGS_SHEET), BEST_BUTTON_PMT, tmp_parent);
 	tmp = tmp->next;
-	tmp->next = button_factory(settings_sheet, CANCEL_BUTTON_PMT, tmp_parent);
+	tmp->next = button_factory(getImage(SETTINGS_SHEET), CANCEL_BUTTON_PMT, tmp_parent);
 	tmp = tmp->next;
-	tmp->next = button_factory(settings_sheet, START_BUTTON_PMT, tmp_parent);
+	tmp->next = button_factory(getImage(SETTINGS_SHEET), START_BUTTON_PMT, tmp_parent);
 
 	return (result);
 }
@@ -97,32 +97,32 @@ gui_chess build_settings_menu()
 {
 	gui_chess result, tmp, tmp_parent;
 
-	result = window_factory(backround);
+	result = window_factory(getImage(BACKGROUND_IMAGE));
 	tmp = result;
-	tmp->child = panel_factory(panels_sheet, FISH_PANEL_PMT, tmp);
+	tmp->child = panel_factory(getImage(PANELS_SHEET), FISH_PANEL_PMT, tmp);
 	tmp = tmp->child;
-	tmp->child = label_factory(settings_sheet, COLOR_LABEL_PMT, tmp);
+	tmp->child = label_factory(getImage(SETTINGS_SHEET), COLOR_LABEL_PMT, tmp);
 	tmp_parent = tmp;
 	tmp = tmp->child;
-	tmp->next = button_factory(settings_sheet, WHITE_S_BUTTON_PMT, tmp_parent);
+	tmp->next = button_factory(getImage(SETTINGS_SHEET), WHITE_S_BUTTON_PMT, tmp_parent);
 	tmp = tmp->next;
-	tmp->next = button_factory(settings_sheet, BLACK_BUTTON_PMT, tmp_parent);
+	tmp->next = button_factory(getImage(SETTINGS_SHEET), BLACK_BUTTON_PMT, tmp_parent);
 	tmp = tmp->next;
-	tmp->next = label_factory(settings_sheet, DIFFICULTY_LABEL_PMT, tmp_parent);
+	tmp->next = label_factory(getImage(SETTINGS_SHEET), DIFFICULTY_LABEL_PMT, tmp_parent);
 	tmp = tmp->next;
-	tmp->next = button_factory(settings_sheet, ONE_S_BUTTON_PMT, tmp_parent);
+	tmp->next = button_factory(getImage(SETTINGS_SHEET), ONE_S_BUTTON_PMT, tmp_parent);
 	tmp = tmp->next;
-	tmp->next = button_factory(settings_sheet, TWO_BUTTON_PMT, tmp_parent);
+	tmp->next = button_factory(getImage(SETTINGS_SHEET), TWO_BUTTON_PMT, tmp_parent);
 	tmp = tmp->next;
-	tmp->next = button_factory(settings_sheet, THREE_BUTTON_PMT, tmp_parent);
+	tmp->next = button_factory(getImage(SETTINGS_SHEET), THREE_BUTTON_PMT, tmp_parent);
 	tmp = tmp->next;
-	tmp->next = button_factory(settings_sheet, FOUR_BUTTON_PMT, tmp_parent);
+	tmp->next = button_factory(getImage(SETTINGS_SHEET), FOUR_BUTTON_PMT, tmp_parent);
 	tmp = tmp->next;
-	tmp->next = button_factory(settings_sheet, BEST_BUTTON_PMT, tmp_parent);
+	tmp->next = button_factory(getImage(SETTINGS_SHEET), BEST_BUTTON_PMT, tmp_parent);
 	tmp = tmp->next;
-	tmp->next = button_factory(settings_sheet, CANCEL_BUTTON_PMT, tmp_parent);
+	tmp->next = button_factory(getImage(SETTINGS_SHEET), CANCEL_BUTTON_PMT, tmp_parent);
 	tmp = tmp->next;
-	tmp->next = button_factory(settings_sheet, START_BUTTON_PMT, tmp_parent);
+	tmp->next = button_factory(getImage(SETTINGS_SHEET), START_BUTTON_PMT, tmp_parent);
 	
 	return (result);
 }
@@ -130,26 +130,26 @@ gui_chess build_settings_menu()
 gui_chess build_save_menu()
 {
 	gui_chess result, tmp, tmp_parent;
-	result = window_factory(backround);
+	result = window_factory(getImage(BACKGROUND_IMAGE));
 	tmp = result;
-	tmp->child = panel_factory(panels_sheet, FISH_PANEL_PMT, tmp);
+	tmp->child = panel_factory(getImage(PANELS_SHEET), FISH_PANEL_PMT, tmp);
 	tmp = tmp->child;
-	tmp->child = label_factory(settings_sheet, SAVE_LABEL_PMT, tmp);
+	tmp->child = label_factory(getImage(SETTINGS_SHEET), SAVE_LABEL_PMT, tmp);
 	tmp_parent = tmp;
 	tmp = tmp->child;
-	tmp->next = button_factory(settings_sheet, ONE_BUTTON_PMT, tmp_parent);
+	tmp->next = button_factory(getImage(SETTINGS_SHEET), ONE_BUTTON_PMT, tmp_parent);
 	tmp = tmp->next;
-	tmp->next = button_factory(settings_sheet, TWO_BUTTON_PMT, tmp_parent);
+	tmp->next = button_factory(getImage(SETTINGS_SHEET), TWO_BUTTON_PMT, tmp_parent);
 	tmp = tmp->next;
-	tmp->next = button_factory(settings_sheet, THREE_BUTTON_PMT, tmp_parent);
+	tmp->next = button_factory(getImage(SETTINGS_SHEET), THREE_BUTTON_PMT, tmp_parent);
 	tmp = tmp->next;
-	tmp->next = button_factory(settings_sheet, FOUR_BUTTON_PMT, tmp_parent);
+	tmp->next = button_factory(getImage(SETTINGS_SHEET), FOUR_BUTTON_PMT, tmp_parent);
 	tmp = tmp->next;
-	tmp->next = button_factory(settings_sheet, FIVE_BUTTON_PMT, tmp_parent);
+	tmp->next = button_factory(getImage(SETTINGS_SHEET), FIVE_BUTTON_PMT, tmp_parent);
 	tmp = tmp->next;
-	tmp->next = button_factory(settings_sheet, CANCEL_BUTTON_PMT, tmp_parent);
+	tmp->next = button_factory(getImage(SETTINGS_SHEET), CANCEL_BUTTON_PMT, tmp_parent);
 	tmp = tmp->next;
-	tmp->next = button_factory(settings_sheet, SAVE_ARROW_PMT, tmp_parent);
+	tmp->next = button_factory(getImage(SETTINGS_SHEET), SAVE_ARROW_PMT, tmp_parent);
 	return (result);
 }
 
@@ -157,26 +157,26 @@ gui_chess build_load_menu()
 {
 	gui_chess result, tmp, tmp_parent;
 
-	result = window_factory(backround);
+	result = window_factory(getImage(BACKGROUND_IMAGE));
 	tmp = result;
-	tmp->child = panel_factory(panels_sheet, FISH_PANEL_PMT, tmp);
+	tmp->child = panel_factory(getImage(PANELS_SHEET), FISH_PANEL_PMT, tmp);
 	tmp = tmp->child;
-	tmp->child = label_factory(settings_sheet, LOAD_LABEL_PMT, tmp);
+	tmp->child = label_factory(getImage(SETTINGS_SHEET), LOAD_LABEL_PMT, tmp);
 	tmp_parent = tmp;
 	tmp = tmp->child;
-	tmp->next = button_factory(settings_sheet, ONE_BUTTON_PMT, tmp_parent);
+	tmp->next = button_factory(getImage(SETTINGS_SHEET), ONE_BUTTON_PMT, tmp_parent);
 	tmp = tmp->next;
-	tmp->next = button_factory(settings_sheet, TWO_BUTTON_PMT, tmp_parent);
+	tmp->next = button_factory(getImage(SETTINGS_SHEET), TWO_BUTTON_PMT, tmp_parent);
 	tmp = tmp->next;
-	tmp->next = button_factory(settings_sheet, THREE_BUTTON_PMT, tmp_parent);
+	tmp->next = button_factory(getImage(SETTINGS_SHEET), THREE_BUTTON_PMT, tmp_parent);
 	tmp = tmp->next;
-	tmp->next = button_factory(settings_sheet, FOUR_BUTTON_PMT, tmp_parent);
+	tmp->next = button_factory(getImage(SETTINGS_SHEET), FOUR_BUTTON_PMT, tmp_parent);
 	tmp = tmp->next;
-	tmp->next = button_factory(settings_sheet, FIVE_BUTTON_PMT, tmp_parent);
+	tmp->next = button_factory(getImage(SETTINGS_SHEET), FIVE_BUTTON_PMT, tmp_parent);
 	tmp = tmp->next;
-	tmp->next = button_factory(settings_sheet, CANCEL_BUTTON_PMT, tmp_parent);
+	tmp->next = button_factory(getImage(SETTINGS_SHEET), CANCEL_BUTTON_PMT, tmp_parent);
 	tmp = tmp->next;
-	tmp->next = button_factory(settings_sheet, LOAD_ARROW_PMT, tmp_parent);
+	tmp->next = button_factory(getImage(SETTINGS_SHEET), LOAD_ARROW_PMT, tmp_parent);
 
 	return (result);
 }
@@ -185,16 +185,16 @@ gui_chess build_game_menu()
 {
 	gui_chess result, tmp, tmp_parent;
 
-	result = window_factory(main_img);
+	result = window_factory(getImage(MAIN_IMG));
 	tmp = result;
 	tmp_parent = result;
-	tmp->child = button_factory(settings_sheet, MENU_BUTTON_PMT, tmp_parent);
+	tmp->child = button_factory(getImage(SETTINGS_SHEET), MENU_BUTTON_PMT, tmp_parent);
 	tmp = tmp->child;
-	tmp->next = button_factory(settings_sheet, SAVE_BUTTON_PMT, tmp_parent);
+	tmp->next = button_factory(getImage(SETTINGS_SHEET), SAVE_BUTTON_PMT, tmp_parent);
 	tmp = tmp->next;
-	tmp->next = button_factory(settings_sheet, RESTART_BUTTON_PMT, tmp_parent);
+	tmp->next = button_factory(getImage(SETTINGS_SHEET), RESTART_BUTTON_PMT, tmp_parent);
 	tmp = tmp->next;
-	tmp->next = button_factory(settings_sheet, QUIT_SIDE_BUTTON_PMT, tmp_parent);
+	tmp->next = button_factory(getImage(SETTINGS_SHEET), QUIT_SIDE_BUTTON_PMT, tmp_parent);
 
 	return (result);
 }
@@ -203,26 +203,26 @@ gui_chess white_player_promots()
 {
 	gui_chess result, tmp, tmp_parent;
 
-	result = window_factory(main_img);
+	result = window_factory(getImage(MAIN_IMG));
 	tmp = result;
 	tmp_parent = result;
-	tmp->child = panel_factory(panels_sheet, FISH_PANEL_PMT, tmp);
+	tmp->child = panel_factory(getImage(PANELS_SHEET), FISH_PANEL_PMT, tmp);
 	tmp = tmp->child;
 	tmp->parent = tmp_parent;
 	tmp_parent = tmp;
-	tmp->child = label_factory(pieces_sheet, PROMOTE_LABEL, tmp);
+	tmp->child = label_factory(getImage(PIECES_SHEET), PROMOTE_LABEL, tmp);
 	tmp = tmp->child;
 	tmp->parent = tmp_parent;
-	tmp->next = button_factory(pieces_sheet, W_ROOK_PMT, (PROMOTE_FIRST_POS_X + PROMOTE_OFFSET), PROMOTE_FIRST_POS_Y, tmp_parent);
+	tmp->next = button_factory(getImage(PIECES_SHEET), W_ROOK_PMT, (PROMOTE_FIRST_POS_X + PROMOTE_OFFSET), PROMOTE_FIRST_POS_Y, tmp_parent);
 	tmp = tmp->next;
 	tmp->parent = tmp_parent;
-	tmp->next = button_factory(pieces_sheet, W_QUEEN_PMT, PROMOTE_FIRST_POS_X, PROMOTE_FIRST_POS_Y, tmp_parent);
+	tmp->next = button_factory(getImage(PIECES_SHEET), W_QUEEN_PMT, PROMOTE_FIRST_POS_X, PROMOTE_FIRST_POS_Y, tmp_parent);
 	tmp = tmp->next;
 	tmp->parent = tmp_parent;
-	tmp->next = button_factory(pieces_sheet, W_KNIGHT_PMT, (PROMOTE_FIRST_POS_X + 3 * PROMOTE_OFFSET), PROMOTE_FIRST_POS_Y, tmp_parent);
+	tmp->next = button_factory(getImage(PIECES_SHEET), W_KNIGHT_PMT, (PROMOTE_FIRST_POS_X + 3 * PROMOTE_OFFSET), PROMOTE_FIRST_POS_Y, tmp_parent);
 	tmp = tmp->next;
 	tmp->parent = tmp_parent;
-	tmp->next = button_factory(pieces_sheet, W_BISHOP_PMT, (PROMOTE_FIRST_POS_X + 2 * PROMOTE_OFFSET), PROMOTE_FIRST_POS_Y, tmp_parent);
+	tmp->next = button_factory(getImage(PIECES_SHEET), W_BISHOP_PMT, (PROMOTE_FIRST_POS_X + 2 * PROMOTE_OFFSET), PROMOTE_FIRST_POS_Y, tmp_parent);
 	tmp = tmp->next;
 	tmp->parent = tmp_parent;
 	return (result);
@@ -232,26 +232,26 @@ gui_chess black_player_promots()
 {
 	gui_chess result, tmp, tmp_parent;
 
-	result = window_factory(main_img);
+	result = window_factory(getImage(MAIN_IMG));
 	tmp = result;
 	tmp_parent = result;
-	tmp->child = panel_factory(panels_sheet, FISH_PANEL_PMT, tmp);
+	tmp->child = panel_factory(getImage(PANELS_SHEET), FISH_PANEL_PMT, tmp);
 	tmp = tmp->child;
 	tmp->parent = tmp_parent;
 	tmp_parent = tmp;
-	tmp->child = label_factory(pieces_sheet, PROMOTE_LABEL, tmp);
+	tmp->child = label_factory(getImage(PIECES_SHEET), PROMOTE_LABEL, tmp);
 	tmp = tmp->child;
 	tmp->parent = tmp_parent;
-	tmp->next = button_factory(pieces_sheet, B_ROOK_PMT, (PROMOTE_FIRST_POS_X + PROMOTE_OFFSET), PROMOTE_FIRST_POS_Y, tmp_parent);
+	tmp->next = button_factory(getImage(PIECES_SHEET), B_ROOK_PMT, (PROMOTE_FIRST_POS_X + PROMOTE_OFFSET), PROMOTE_FIRST_POS_Y, tmp_parent);
 	tmp = tmp->next;
 	tmp->parent = tmp_parent;
-	tmp->next = button_factory(pieces_sheet, B_QUEEN_PMT, PROMOTE_FIRST_POS_X, PROMOTE_FIRST_POS_Y, tmp_parent);
+	tmp->next = button_factory(getImage(PIECES_SHEET), B_QUEEN_PMT, PROMOTE_FIRST_POS_X, PROMOTE_FIRST_POS_Y, tmp_parent);
 	tmp = tmp->next;
 	tmp->parent = tmp_parent;
-	tmp->next = button_factory(pieces_sheet, B_KNIGHT_PMT, (PROMOTE_FIRST_POS_X + 3 * PROMOTE_OFFSET), PROMOTE_FIRST_POS_Y, tmp_parent);
+	tmp->next = button_factory(getImage(PIECES_SHEET), B_KNIGHT_PMT, (PROMOTE_FIRST_POS_X + 3 * PROMOTE_OFFSET), PROMOTE_FIRST_POS_Y, tmp_parent);
 	tmp = tmp->next;
 	tmp->parent = tmp_parent;
-	tmp->next = button_factory(pieces_sheet, B_BISHOP_PMT, (PROMOTE_FIRST_POS_X + 2 * PROMOTE_OFFSET), PROMOTE_FIRST_POS_Y, tmp_parent);
+	tmp->next = button_factory(getImage(PIECES_SHEET), B_BISHOP_PMT, (PROMOTE_FIRST_POS_X + 2 * PROMOTE_OFFSET), PROMOTE_FIRST_POS_Y, tmp_parent);
 	tmp = tmp->next;
 	tmp->parent = tmp_parent;
 	return (result);
