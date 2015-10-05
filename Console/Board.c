4fe1ed623fc1dc025599633fce1acf62736dcc87
Ran Feder <ranfeder@outlook.com>
2015-10-05 18:30:02 +0300

    fix printBoard whitespace

--- Console/Board.c
+++ Console/Board.c
@@ -3,16 +3,16 @@
 void printBoard(char** board) {
 	int i,j;
 	printBoardLineSeperator();
-	for (j = BOARD_SIZE-1; j >= 0 ; j--)
+	for (j = BOARD_SIZE - 1; j >= 0 ; j--)
 	{
-		printf((j < 9 ? " %d" : "%d"), j+1);
+		printf("%d", j + 1);
 		for (i = 0; i < BOARD_SIZE; i++){
 			printf("| %c ", board[i][j]);
 		}
 		printf("|\n");
 		printBoardLineSeperator();
 	}
-	printf("   ");
+	printf("  ");
 	for (j = 0; j < BOARD_SIZE; j++){
 		printf(" %c  ", (char)('a' + j));
 	}
@@ -21,8 +21,8 @@ void printBoard(char** board) {
 
 void printBoardLineSeperator() {
 	int i;
-	printf("  |");
-	for (i = 1; i < BOARD_SIZE*4; i++){
+	printf(" |");
+	for (i = 1; i < BOARD_SIZE * 4; i++){
 		printf("-");
 	}
 	printf("|\n");
